4e36895b1dd065f709648a2ef0f449068dc523c0
Fabian Mendez <fmendgra@gmail.com>
2025-08-26 10:04:39 -0600

    se sgrega config

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: "sql-scada-service",
+      script: "./src/app.js",
+      env: {
+        NODE_ENV: "development",
+        SQL_USER: "grafana",
+        SQL_PASSWORD: "Monitoreo2026**",
+        SQL_SERVER: "172.16.1.206",
+        SQL_DATABASE: "GeneracionBD",
+        SQL_PORT: "1433",
+        REDIS_HOST: "localhost",
+        REDIS_PORT: "6379"
+      }
+    }
+  ]
+};
