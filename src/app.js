c7502b948e697c99236526a5f70f9115b12c296a
Fabian Mendez <fmendgra@gmail.com>
2025-08-26 11:52:51 -0600

    cambios en static

--- src/app.js
+++ src/app.js
@@ -13,9 +13,9 @@ app.use(utc6Middleware);
 
 // 👉 Servir carpeta "public" como estática
 // Cualquier archivo dentro de /public será accesible en /static/
-app.use('/static', express.static(path.join(__dirname, '../public')));
+app.use('/static', express.static(path.join(__dirname, '../public/')));
 
-console.log("Sirviendo estáticos desde:", path.join(__dirname, '../public'));
+console.log("Sirviendo estáticos desde:", path.join(__dirname, '../public/'));
 
 
 // Proporciona la ruta absoluta de la carpeta 'public'
